96c58cad762161a2c754c05f04410cdd7dbe752b
PaulBerkersAvans <130966922+PaulBerkersAvans@users.noreply.github.com>
2025-02-17 15:42:05 +0100

    added ESlint

--- PaulsBoekenClubMembers/app.js
+++ PaulsBoekenClubMembers/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var __dirname = path.resolve();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -28,7 +29,8 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+//app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
