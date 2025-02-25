24f4d53ae6cbb2c18eb2a33534d77fc0f806e01e
PaulBerkersAvans <130966922+PaulBerkersAvans@users.noreply.github.com>
2025-02-25 15:54:27 +0100

    first docker compose

--- PaulsBoekenClubMembers/services/database.js
+++ PaulsBoekenClubMembers/services/database.js
@@ -3,11 +3,20 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URL;
 
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 
-const db = client.db(process.env.DB_NAME);
+// const db = client.db(process.env.DB_NAME);
 
-module.exports = {
-    db: db,
-    client: client
-};
+// module.exports = {
+//     db: db,
+//     client: client
+// };
+
+if (!uri) {
+    throw new Error("MONGO_URL is not defined. Check your environment variables!");
+}
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = client.db(process.env.DB_NAME || "membersDB");
+
+module.exports = { db, client };
